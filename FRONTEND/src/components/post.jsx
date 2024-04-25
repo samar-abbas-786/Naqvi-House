8d0f34cb414a2b17bf7509bce38a0a122e37df1b
Samar Abbas <samarabbas172003@gmail.com>
2024-04-25 18:15:30 +0530

    post

--- FRONTEND/src/components/post.jsx
+++ FRONTEND/src/components/post.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import "../App.css";
 
 const post = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <div className="box h-[80vh] w-screen bg-blue-200 flex flex-col  justify-center items-center">
+        <h1 className="mb-[4rem] text-4xl text-[#262626] font-semibold ">
+          Lets Create a Post
+        </h1>
+        <form
+          action=""
+          className=" inside-input h-[50vh] w-[90vw] bg-white rounded-lg flex flex-col justify-around items-center "
+        >
+          <input name="image" type="file" placeholder="Post Your Image" />
+          <input
+            name="title"
+            type="text"
+            placeholder="Enter the Title of Post"
+          />
+          <input
+            name="description"
+            type="text"
+            placeholder="Enter the Description of Post"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default post;
